3bde7b6bcee8b92351ff0899f45edbf9a9eb2ce7
Nam Ta <nam_ta_101@hotmail.com>
2022-08-17 20:13:18 +0100

    test front page edit

---
+++ src/components/activities/ActivityItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+const ActivityItem = ({}) => {
+  return (
+    <React.Fragment>
+      <Text>HI NAM</Text>
+    </React.Fragment>
+  );
+};
+
+export default ActivityItem;
